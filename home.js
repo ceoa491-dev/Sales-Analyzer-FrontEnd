7f2c6c325565fdd5e2abab20af88651c16692437
Dinesh <ceoa491@gmail.com>
2026-04-14 22:45:21 +0530

    Home User Info

---
+++ home.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+    let name=localStorage.getItem("name");
+    document.getElementById("ac").addEventListener("click",function(e){
+        e.preventDefault();
+        document.getElementById("namel").innerText=name;
+    })
+}
