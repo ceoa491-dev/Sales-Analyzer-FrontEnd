7e1fd1642929b11486a118962d7dc4cd3988d51e
Dinesh <ceoa491@gmail.com>
2026-04-14 21:36:33 +0530

    Login Changes And Storing User Info And Redirection

--- login.js
+++ login.js
@@ -13,8 +13,17 @@ document.getElementById("lg").addEventListener("submit",function(e){
     })
     .then(res=>res.json())
     .then(data=>{
-        console.log(data)
+        if(data.status=="success"){
+            console.log(data)
         alert("Logged Successfull")
+        localStorage.setItem("email",data.email)
+        localStorage.setItem("name",data.name)
+        localStorage.setItem("ph",data.ph)
+        window.location.href="home.html"
+        }
+        else{
+            alert("Login Failed")
+        }
     })
     .catch(err=>{
         console.error(err)
